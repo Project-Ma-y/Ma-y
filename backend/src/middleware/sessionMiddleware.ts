d6ec2efb52142ebf5562dfea1edef7f2f33d86ca
dayeongkim999 <kimdy4162@gmail.com>
2025-08-14 13:22:24 +0900

    update for test10

--- backend/src/middleware/sessionMiddleware.ts
+++ backend/src/middleware/sessionMiddleware.ts
@@ -2,14 +2,18 @@ import { Request, Response, NextFunction } from "express";
 import { db } from "../utils/firebase";
 import { initSession } from "../services/sessionService";
 
+const skipPaths = ["/api/sessions/main"];
 
 export const loadSession = async (req: Request, res: Response, next: NextFunction) => {
   let sessionId = req.cookies.sessionId;
 
   //console.log("loadSession 시작"); //test
   //console.log(sessionId);
-  //세션 없을 시 생성
-  if (!sessionId){
+  // 세션id 있고, 스킵패스임 => 그럼 init생성 x
+  // 세션 id 있고, 스킵패스가 아님 => 그럼 init 생성x
+  // 세션 id 없고, 스킵패스임  => 그럼 init 생성x
+  // 세션 id 없고,  스킵패스가 아님 => 그럼 init 생성
+  if (!sessionId && !skipPaths.includes(req.path)){
     console.log("loadSession에서 세션id 못찾음"); //test
     sessionId = await initSession(req.user?.uid || "");
     res.cookie("sessionId", sessionId, { httpOnly: true, secure: true, sameSite: "none" }); //test
