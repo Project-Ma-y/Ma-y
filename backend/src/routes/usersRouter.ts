14ae0560a87466c9b91841decef7376eefeeb6b5
dayeongkim999 <kimdy4162@gmail.com>
2025-08-25 11:32:28 +0900

    update gitpages and router

--- backend/src/routes/usersRouter.ts
+++ backend/src/routes/usersRouter.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { verifyFirebaseToken } from "../middleware/authMiddleware";
 import { loadSession, noCache } from "../middleware/sessionMiddleware";
-
+import { getAllUsers, getUserById, deleteUser } from "../controllers/usersController";
 
 const router = Router();
 
-router.get("/users", verifyFirebaseToken, loadSession, ); //가입자 집계
-router.get("/users/:id", verifyFirebaseToken, loadSession, ); //회원 상세 정보 조회
-router.delete("/users/:id", verifyFirebaseToken, loadSession, ); //회원 삭제
+router.get("/", verifyFirebaseToken, loadSession, getAllUsers); //가입자 집계
+router.get("/:id", verifyFirebaseToken, loadSession, getUserById); //회원 상세 정보 조회
+router.delete("/:id", verifyFirebaseToken, loadSession, deleteUser); //회원 삭제
 
 export default router;
  
