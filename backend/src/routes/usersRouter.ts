54b6579c6e60b00435dbf1640ac16edca2963389
dayeongkim999 <kimdy4162@gmail.com>
2025-08-25 18:02:58 +0900

    complete checkAdmin api

--- backend/src/routes/usersRouter.ts
+++ backend/src/routes/usersRouter.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { verifyFirebaseToken } from "../middleware/authMiddleware";
 import { loadSession, noCache } from "../middleware/sessionMiddleware";
 import { getAllUsers, getUserById, deleteUser } from "../controllers/usersController";
+import { checkAdminUid } from "../middleware/checkAdminUid";
 
 const router = Router();
 
-router.get("/", verifyFirebaseToken, loadSession, getAllUsers); //가입자 집계
-router.get("/:id", verifyFirebaseToken, loadSession, getUserById); //회원 상세 정보 조회
-router.delete("/:id", verifyFirebaseToken, loadSession, deleteUser); //회원 삭제
+router.get("/", verifyFirebaseToken, loadSession, checkAdminUid, getAllUsers); //가입자 집계
+router.get("/:id", verifyFirebaseToken, loadSession, checkAdminUid, getUserById); //회원 상세 정보 조회
+router.delete("/:id", verifyFirebaseToken, loadSession, checkAdminUid, deleteUser); //회원 삭제
 
 export default router;
  
