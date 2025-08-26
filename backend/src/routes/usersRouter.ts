91134fd065cabac951d2abb3d7dd459b0243d6fe
dayeongkim999 <kimdy4162@gmail.com>
2025-08-26 20:30:30 +0900

    edit usersRouter2

--- backend/src/routes/usersRouter.ts
+++ backend/src/routes/usersRouter.ts
@@ -6,11 +6,11 @@ import { checkAdminUid } from "../middleware/checkAdminUid";
 
 const router = Router();
 
-router.get("/:id", verifyFirebaseToken, loadSession, checkAdminUid, getUserByUID); //회원 상세 정보 조회
+router.get("/me", verifyFirebaseToken, loadSession, getMyProfile); //내 정보 보기
 router.put("/:id", verifyFirebaseToken, loadSession, updateUser); //정보 업데이트
+router.get("/:id", verifyFirebaseToken, loadSession, checkAdminUid, getUserByUID); //회원 상세 정보 조회
 router.delete("/:id", verifyFirebaseToken, loadSession, checkAdminUid, deleteUser); //회원 삭제
 
-router.get("/me", verifyFirebaseToken, loadSession, getMyProfile); //내 정보 보기
 router.get("/", verifyFirebaseToken, loadSession, checkAdminUid, getAllUsers); //가입자 집계
 
 export default router;
