f3e376df8bcd77681edb09ce51f5a1cf84174c06
dayeongkim999 <kimdy4162@gmail.com>
2025-08-28 16:17:20 +0900

    complete getApplicationReachService

--- backend/src/services/statsService.ts
+++ backend/src/services/statsService.ts
@@ -14,7 +14,7 @@ export const getSignupConversionService = async () => {
     //날짜별 집계를 담을 Map
     const statsMap = new Map<
       string,
-      { sessions: number; registeredSessions: number }
+      { sessions: number; signupSessions: number }
     >();
 
     snapshot.forEach((doc: any) => {
@@ -24,13 +24,13 @@ export const getSignupConversionService = async () => {
       if (!data.createdAt) return;
       const date = data.createdAt.split("T")[0];
       if (!statsMap.has(date)) {
-        statsMap.set(date, { sessions: 0, registeredSessions: 0 });
+        statsMap.set(date, { sessions: 0, signupSessions: 0 });
       }
 
       const entry = statsMap.get(date)!;
       entry.sessions += 1;
       if (data.isRegistered) {
-        entry.registeredSessions += 1;
+        entry.signupSessions += 1;
       }
     })
 
@@ -44,15 +44,15 @@ export const getSignupConversionService = async () => {
 
     //누적(total) 값 추가
     let totalSessions = 0;
-    let totalRegisteredSessions = 0;
+    let totalSignupSessions = 0;
 
     result = result.map((entry) => {
       totalSessions += entry.sessions;
-      totalRegisteredSessions += entry.registeredSessions;
+      totalSignupSessions += entry.signupSessions;
       return {
         ...entry,
         totalSessions,
-        totalRegisteredSessions,
+        totalRegisteredSessions: totalSignupSessions,
       };
     });
 
@@ -63,22 +63,65 @@ export const getSignupConversionService = async () => {
   }
 };
 
+
 // 동행 신청 페이지 도달율
 // 동행 신청 페이지 도달 횟수가 1 이상인 id 개수 / 세션 id 개수
 export const getApplicationReachService = async () => {
   try {
-    //세션 id 개수
-    const snapshotA = await collectionRef.count().get();
-    //동행 신청 페이지 도달 횟수가 1 이상인 id 개수
-    const snapshotB = await collectionRef.where('visitApplyPageCount', '>', 0).count().get();
+    const snapshot = await collectionRef.get();
 
-    const applicationReach = snapshotB.data().count / snapshotA.data().count;
-    return applicationReach;
+    //날짜별 집계를 담을 Map
+    const statsMap = new Map<
+      string,
+      { sessions: number; reachApplypageSessions: number }
+    >();
+
+    snapshot.forEach((doc: any) => {
+      const data = doc.data();
+
+      //날짜
+      if (!data.createdAt) return;
+      const date = data.createdAt.split("T")[0];
+      if (!statsMap.has(date)) {
+        statsMap.set(date, { sessions: 0, reachApplypageSessions: 0 });
+      }
+
+      const entry = statsMap.get(date)!;
+      entry.sessions += 1;
+      if (data.isRegistered) {
+        entry.reachApplypageSessions += 1;
+      }
+    })
+
+    // Map → Array 변환 & 날짜순 정렬
+    let result = Array.from(statsMap.entries()).map(([date, value]) => ({
+      date,
+      ...value,
+    }));
+    result.sort((a, b) => a.date.localeCompare(b.date));
+
+
+    //누적(total) 값 추가
+    let totalSessions = 0;
+    let totalReachApplypageSessions = 0;
+
+    result = result.map((entry) => {
+      totalSessions += entry.sessions;
+      totalReachApplypageSessions += entry.reachApplypageSessions;
+      return {
+        ...entry,
+        totalSessions,
+        totalRegisteredSessions: totalReachApplypageSessions,
+      };
+    });
+
+    return result;
   } catch (error) {
     console.error("❌ getApplicationReachService 오류:", error);
     throw new Error("statsService: getApplicationReachService 오류 발생");
   }
 };
+
 
 // 동행 신청 전환율
 // 동행 신청 완료 회수가 1 이상인 세션 id 개수 / 세션 id 개수
