fecc9c72e0f77a2897f7e089634860febe2a0b0d
jinjintv <theffffff@naver.com>
2025-08-31 02:29:25 +0900

    dd

--- frontend/src/lib/endpoints.ts
+++ frontend/src/lib/endpoints.ts
@@ -2,5 +2,5 @@ export const ENDPOINTS = {
   signupEmail: "/auth/signupEmail",
   login: "/api/auth/login",
   logout: "/api/auth/logout", // '/auth/logout' -> '/api/auth/logout'으로 변경
-  me: "/users/me", // '/auth/check-login' -> '/api/users/me'로 변경
+  me: "/users/me/profile", // '/auth/check-login' -> '/api/users/me'로 변경
 } as const;
